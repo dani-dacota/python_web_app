e2967c8ce64e912a378fc899a5f8e80a6a647047
Danial Siddiqi <danialsiddiqi@Ardents-iMac.local>
2019-05-04 13:48:05 -0700

    add sqlite3 db

--- myWebsite/settings.py
+++ myWebsite/settings.py
@@ -78,18 +78,18 @@ WSGI_APPLICATION = 'myWebsite.wsgi.application'
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    # }
     'default': {
-    'ENGINE': 'django.db.backends.postgresql',
-    'NAME': 'duu0kkhdb2v0t',
-    'USER': 'uhwsfcejyjerhc',
-    'PASSWORD': '1c2288f228fe773564774e4f8c5dfa42e056b102e3166053fea7c15400217d0b',
-    'HOST': 'ec2-54-83-205-27.compute-1.amazonaws.com',
-    'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
+    # 'default': {
+    # 'ENGINE': 'django.db.backends.postgresql',
+    # 'NAME': 'duu0kkhdb2v0t',
+    # 'USER': 'uhwsfcejyjerhc',
+    # 'PASSWORD': '1c2288f228fe773564774e4f8c5dfa42e056b102e3166053fea7c15400217d0b',
+    # 'HOST': 'ec2-54-83-205-27.compute-1.amazonaws.com',
+    # 'PORT': '5432',
+    # }
 }
 
 
